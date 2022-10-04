c8c70df34a0877e9ecef4cc8518f5aca7c7908f2
willianAD <willian.afonso013@gmail.com>
2022-10-04 10:58:38 -0300

    Requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends React.Component {
   constructor() {
@@ -8,6 +11,9 @@ class Search extends React.Component {
     this.state = {
       artista: '',
       button: true,
+      lista: [],
+      isLoading: false,
+      copia: '',
     };
   }
 
@@ -25,8 +31,25 @@ class Search extends React.Component {
     });
   };
 
+  clickButton = async () => {
+    this.setState({
+      isLoading: true,
+    });
+    const { artista } = this.state;
+    const recebe = await searchAlbumsAPI(artista);
+    this.setState({
+      copia: artista,
+      isLoading: false,
+      lista: recebe,
+      artista: '',
+    });
+  };
+
   render() {
-    const { artista, button } = this.state;
+    const { artista, button, isLoading, lista, copia } = this.state;
+    if (isLoading) {
+      return <Loading />;
+    }
     return (
       <div data-testid="page-search">
         <Header />
@@ -44,9 +67,29 @@ class Search extends React.Component {
           data-testid="search-artist-button"
           type="button"
           disabled={ button }
+          onClick={ this.clickButton }
         >
           Pesquisar
         </button>
+        <div>
+          {
+            lista.length > 0
+              ? <p>{`Resultado de álbuns de: ${copia}`}</p>
+              : <p>Nenhum álbum foi encontrado</p>
+          }
+          { lista.map((album) => (
+            <div key={ album.collectionId }>
+              <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+              <h3>{ album.collectionName }</h3>
+              <p>{ album.artistName }</p>
+              <Link
+                to={ `/album/${album.collectionId}` }
+                data-testid={ `link-to-album-${album.collectionId}` }
+              />
+            </div>
+          ))}
+
+        </div>
       </div>
     );
   }
